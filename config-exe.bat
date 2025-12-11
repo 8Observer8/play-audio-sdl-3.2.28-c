26f6b57278e571442c553aa6dc83468b21b124c4
Ivan Enzhaev <8observer8@gmail.com>
2025-12-11 23:34:13 +0400

    Set -DCMAKE_BUILD_TYPE to Debug

--- config-exe.bat
+++ config-exe.bat
@@ -1,4 +1,4 @@
 cmake -G "MinGW Makefiles" -S . -B dist/win ^
 -DSDL3_DIR=H:/libs/SDL3-devel-3.2.28-mingw/lib/cmake/SDL3 ^
 -DSDL3_mixer_DIR=H:/libs/SDL3_mixer-devel-3.0.0-mingw/lib/cmake/SDL3_mixer ^
--DCMAKE_BUILD_TYPE=Release
+-DCMAKE_BUILD_TYPE=Debug
